2c2306c1e3083f18e4e932349a3fa1a0069eebd9
Akul Bansal <akulbansal@dhcp-10-105-118-62.wireless.northwestern.private>
2023-09-30 17:30:30 -0500

    changed the way threads are set in detr eq

--- zQmip/src/deterministic_equivalent.jl
+++ zQmip/src/deterministic_equivalent.jl
@@ -234,7 +234,9 @@ function deterministic_equivalent(
         )
     end
     # Step 2: create a extensive-form JuMP model and add subproblems.
-    model = optimizer === nothing ? JuMP.Model() : JuMP.Model(optimizer_with_attributes(optimizer,  "Threads" => solver_threads))
+    # model = optimizer === nothing ? JuMP.Model() : JuMP.Model(optimizer_with_attributes(optimizer,  "Threads" => solver_threads))
+    model = optimizer === nothing ? JuMP.Model() : JuMP.Model(optimizer)
+    JuMP.set_attribute(model, "Threads", solver_threads)
     JuMP.set_objective_sense(model, pg.objective_sense)
     for child in tree.children
         add_scenario_to_ef(model, child, check_time_limit)
