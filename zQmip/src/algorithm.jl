c318840b478998022c7e1b7f394749a98f0d7389
Akul Bansal <akulbansal@dhcp-10-105-209-26.wireless.northwestern.private>
2024-01-12 14:17:40 -0600

    update

--- zQmip/src/algorithm.jl
+++ zQmip/src/algorithm.jl
@@ -1638,10 +1638,10 @@ function train(
     if final_run
         fpass = DefaultNestedForwardPass(final_run = final_run)
         # TimerOutputs.@timeit model.timer_output "forward_pass" begin
-        forward_trajectory = forward_pass(model, options, fpass)
-        options.forward_pass_callback(forward_trajectory)
-        # end
-        ub_final = forward_trajectory.cumulative_value
+        # forward_trajectory = forward_pass(model, options, fpass)
+        # options.forward_pass_callback(forward_trajectory)
+        # # end
+        # ub_final = forward_trajectory.cumulative_value
     end 
 
     #=====================================================================#
